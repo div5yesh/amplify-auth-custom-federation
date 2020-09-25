5a174a4b6e8cbded50859ac9d9b9f127a96a3f0b
Divyesh Chitroda <div5yesh@gmail.com>
2020-09-24 20:16:20 -0400

    test custom IdP and state for federated signin

--- src/App.js
+++ src/App.js
@@ -29,6 +29,9 @@ function App(props) {
        if (payload.event === 'signOut') {
          console.log('a user has signed out!')
        }
+       if(payload.event === "customOAuthState"){
+         console.log(payload.data)
+       }
     })
   }, [])
   return (
@@ -39,11 +42,19 @@ function App(props) {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <button onClick={() => Auth.federatedSignIn({customState:`http://localhost:3000?test`})}>Sign In with custom State</button>
+
         <button onClick={checkUser}>Check User</button>
         <button onClick={signOut}>Sign Out</button>
-        <button onClick={() => Auth.federatedSignIn({customProvider: 'Auth0', customState: {}})}>Sign In with Auth0</button>
-        <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Sign In with Google</button>
 
+        <button onClick={() => Auth.federatedSignIn({ provider: "Auth0",})}>Sign In with Auth0</button>
+        <button onClick={() => Auth.federatedSignIn({ provider: "Auth0", customState: window.location.href})}>Sign In with Auth0 and custom state</button>
+
+        <button onClick={() => Auth.federatedSignIn({customProvider: 'Auth0'})}>Sign In with Auth0 custom IdP</button>
+        <button onClick={() => Auth.federatedSignIn({customProvider: 'Auth0', customState: ""})}>Sign In with Auth0 custom IdP and state</button>
+
+        <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Sign In with Google</button>
+        <button onClick={() => Auth.federatedSignIn({provider: 'Google', customState:""})}>Sign In with Google and custom state</button>
       </header>
     </div>
   );
